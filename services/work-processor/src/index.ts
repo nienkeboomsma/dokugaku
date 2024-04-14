4035a41517efc82fcfd74ee700ae5f2c83a4b738
Nienke Boomsma <12957143+nienkeboomsma@users.noreply.github.com>
2024-04-14 21:55:15 +0200

    feat: add cors to work-processor

--- services/work-processor/src/index.ts
+++ services/work-processor/src/index.ts
@@ -1,4 +1,5 @@
 import express, { type Express } from 'express'
+import cors from 'cors'
 import { processMangaFiles, processNovelFiles } from './multer.js'
 import { validateMangaFiles, validateNovelFiles } from './middleware.js'
 import { processManga } from './processManga.js'
@@ -7,7 +8,11 @@ import { processNovel } from './processNovel.js'
 async function main() {
   const app: Express = express()
 
+  app.use(cors())
   app.use(express.json())
+
+  // TODO: look into image optimisation for cover images (both novel and manga);
+  //       for a start, save a smaller copy of img0001.webp as cover.webp
 
   app.post('/processManga', processMangaFiles, validateMangaFiles, processManga)
 
