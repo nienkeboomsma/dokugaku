97753243e86157f8315422f94eea12f58cea781b
Nienke <12957143+nienkeboomsma@users.noreply.github.com>
2024-05-15 09:47:19 +0200

    fix: browse page broke on build without cache

--- apps/web/src/app/browse/page.tsx
+++ apps/web/src/app/browse/page.tsx
@@ -4,12 +4,6 @@ import { getWorkCards } from '../../graphql/queries/getWorkCards'
 // TODO: if necessary: cache the knownVocab value
 // TODO: add pagination
 
-// required to skip SSG during build; without this line, build will fail if
-// there is no pre-existing .next cache and it can't get a GQL response, both
-// of which are the case when a user first uses docker compose up
-// TODO: investigate why this is only an issue for this specific page
-export const dynamic = "force-dynamic";
-
 export default async function Browse() {
   const initialWorkCards = await getWorkCards()
 
