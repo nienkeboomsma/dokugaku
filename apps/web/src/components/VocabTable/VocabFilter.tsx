e788c1b7226378734bd984f1862235366140f289
Nienke Boomsma <12957143+nienkeboomsma@users.noreply.github.com>
2024-05-26 15:49:10 +0200

    feat: add minPageNumber support to VocabFilter

--- apps/web/src/components/VocabTable/VocabFilter.tsx
+++ apps/web/src/components/VocabTable/VocabFilter.tsx
@@ -3,18 +3,25 @@ import { Box, NumberInput, Switch, Text } from '@mantine/core'
 
 import classes from './VocabFilter.module.css'
 import { VocabTableType } from './VocabTable'
+import { ListType } from './VocabSort'
 
 export default function VocabFilter({
+  listType,
   minFrequency,
+  minPageNumber,
   setMinFrequency,
+  setMinPageNumber,
   setShowIgnored,
   setShowUnignored,
   showIgnored,
   showUnignored,
   vocabTableType,
 }: {
+  listType?: ListType
   minFrequency: string | number
+  minPageNumber: string | number
   setMinFrequency: Dispatch<SetStateAction<string | number>>
+  setMinPageNumber: Dispatch<SetStateAction<string | number>>
   setShowIgnored: Dispatch<SetStateAction<boolean>>
   setShowUnignored: Dispatch<SetStateAction<boolean>>
   showIgnored: boolean
@@ -37,16 +44,31 @@ export default function VocabFilter({
           />
         </div>
       )}
-      <div>
-        <Text fw={500} lh={1} mb='xs' mt='-0.25' size='sm'>
-          Minimum frequency
-        </Text>
-        <NumberInput
-          min={1}
-          onChange={setMinFrequency}
-          size='xs'
-          value={minFrequency}
-        />
+      <div className={classes.minimumContainer}>
+        {listType === ListType.Glossary && (
+          <div>
+            <Text fw={500} lh={1} mb='xs' mt='-0.25' size='sm'>
+              Minimum page number
+            </Text>
+            <NumberInput
+              min={1}
+              onChange={setMinPageNumber}
+              size='xs'
+              value={minPageNumber}
+            />
+          </div>
+        )}
+        <div>
+          <Text fw={500} lh={1} mb='xs' mt='-0.25' size='sm'>
+            Minimum frequency
+          </Text>
+          <NumberInput
+            min={1}
+            onChange={setMinFrequency}
+            size='xs'
+            value={minFrequency}
+          />
+        </div>
       </div>
     </Box>
   )
