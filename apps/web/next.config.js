744136a4a5bdaadcb5a3897df24fccfc555738f6
Nienke Boomsma <12957143+nienkeboomsma@users.noreply.github.com>
2024-11-23 17:49:12 +0100

    feat: turn off keepalive to ensure fresh data within first 5s as well

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  httpAgentOptions: {
+    keepAlive: false,
+  },
   output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
