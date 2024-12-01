236b7c16d60fc6e2175beaa49d0ca349c70920af
Nienke Boomsma <12957143+nienkeboomsma@users.noreply.github.com>
2024-12-01 13:22:18 +0100

    fix: don't save vocab filter preferences to local storage

--- apps/web/src/hooks/useVocabTableFilters.ts
+++ apps/web/src/hooks/useVocabTableFilters.ts
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import { useLocalStorage } from '@mantine/hooks'
 import { GQL_ReadStatus } from '@repo/graphql-types'
 
-import {
-  type VocabTableProps,
-  VocabTableType,
-} from '../components/VocabTable/VocabTable'
+import { type VocabTableProps } from '../components/VocabTable/VocabTable'
 import type { SeriesInfo } from '../types/SeriesInfo'
 import type { WorkInfo } from '../types/WorkInfo'
 import { ListType } from '../components/VocabTable/VocabFilter/VocabSort'
@@ -15,19 +11,6 @@ import {
   JlptOptions,
   MinimumValues,
 } from '../components/VocabTable/VocabFilter/VocabFilter'
-
-const getLocalStorageKeySuffix = (props: VocabTableProps) => {
-  switch (props.type) {
-    case VocabTableType.SeriesOrWork:
-      return props.seriesOrWork.id
-    case VocabTableType.Recommended:
-      return 'recommended'
-    case VocabTableType.Known:
-      return 'known'
-    case VocabTableType.Excluded:
-      return 'excluded'
-  }
-}
 
 const getFirstUnreadVolume = (
   seriesOrWork: SeriesInfo | WorkInfo | undefined
@@ -85,35 +68,25 @@ export default function useVocabTableFilters(props: VocabTableProps) {
     getInitialListType(seriesOrWork)
   )
 
-  const [showIgnoredOptions, setShowIgnoredOptions] =
-    useLocalStorage<IgnoredOptions>({
-      defaultValue: {
-        ignored: { checked: true, label: 'Ignored' },
-        unignored: { checked: true, label: 'Unignored' },
-      },
-      key: `DOKUGAKU_SHOW_IGNORED-${getLocalStorageKeySuffix(props)}`,
-    })
-
-  const [showJlptOptions, setShowJlptOptions] = useLocalStorage<JlptOptions>({
-    defaultValue: {
-      N1: { checked: true },
-      N2: { checked: true },
-      N3: { checked: true },
-      N4: { checked: true },
-      N5: { checked: true },
-      nonJlpt: { checked: true, label: 'Non-JLPT' },
-    },
-    key: `DOKUGAKU_SHOW_JLPT-${getLocalStorageKeySuffix(props)}`,
+  const [showIgnoredOptions, setShowIgnoredOptions] = useState<IgnoredOptions>({
+    ignored: { checked: false, label: 'Ignored' },
+    unignored: { checked: true, label: 'Unignored' },
   })
 
-  const [minimumValues, setMinimumValues] = useLocalStorage<MinimumValues>({
-    defaultValue: {
-      volume: getFirstUnreadVolume(seriesOrWork)?.volumeNumber ?? 1,
-      page: getInitialMinPageNumber(seriesOrWork),
-      frequency: 1,
-    },
-    key: `DOKUGAKU_MINIMUM_VALUES-${getLocalStorageKeySuffix(props)}`,
+  const [showJlptOptions, setShowJlptOptions] = useState<JlptOptions>({
+    N1: { checked: true },
+    N2: { checked: true },
+    N3: { checked: true },
+    N4: { checked: true },
+    N5: { checked: true },
+    nonJlpt: { checked: true, label: 'Non-JLPT' },
   })
+
+  const [minimumValues, setMinimumValues] = useState<MinimumValues>(() => ({
+    volume: getFirstUnreadVolume(seriesOrWork)?.volumeNumber ?? 1,
+    page: getInitialMinPageNumber(seriesOrWork),
+    frequency: 1,
+  }))
 
   const [searchValue, setSearchValue] = useState('')
 
