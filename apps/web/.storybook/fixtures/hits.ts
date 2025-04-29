3c263ed8fd2a24d69188d357d4318400db69c98d
Nienke Boomsma <12957143+nienkeboomsma@users.noreply.github.com>
2025-04-29 20:03:33 +0200

    chore: add SearchCorpusPage story

---
+++ apps/web/.storybook/fixtures/hits.ts
@@ -0,0 +1,64 @@
+import type { CorpusSearchResult } from '../../src/types/CorpusSearchResult'
+
+export const mockHits: CorpusSearchResult[] = [
+  {
+    id: '65b4235a-f133-4f07-92a7-1df9b54084bc',
+    title: 'クレヨンしんちゃん 3',
+    allHits: {
+      hitCount: 37,
+      url: '#',
+    },
+    readHits: {
+      hitCount: 0,
+      url: '#',
+    },
+  },
+  {
+    id: '20dd83f3-cfce-498e-b8f9-64562a7f1384',
+    title: 'クレヨンしんちゃん 4',
+    allHits: {
+      hitCount: 8,
+      url: '#',
+    },
+    readHits: {
+      hitCount: 0,
+      url: '#',
+    },
+  },
+  {
+    id: 'a1f5e015-415a-4031-9c62-1674e1221c88',
+    title: 'コンビニ人間',
+    allHits: {
+      hitCount: 1,
+      url: '#',
+    },
+    readHits: {
+      hitCount: 1,
+      url: '#',
+    },
+  },
+  {
+    id: '43b58d3f-dc4a-46a7-b866-bf5131a628de',
+    title: 'セーラー服と機関銃',
+    allHits: {
+      hitCount: 29,
+      url: '#',
+    },
+    readHits: {
+      hitCount: 12,
+      url: '#',
+    },
+  },
+  {
+    id: '03b16794-4472-4ac3-b018-537398f83332',
+    title: '水たまりで息をする',
+    allHits: {
+      hitCount: 5,
+      url: '#',
+    },
+    readHits: {
+      hitCount: 2,
+      url: '#',
+    },
+  },
+]
